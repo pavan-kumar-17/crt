8781bbd94d09e1db8ac6c5603fc3f41b94111cd7
pavan-kumar-17 <23jr1a43i7@gmail.com>
2026-06-04 16:01:56 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,39 +1,39 @@
-# cartoon=['Tom and jerry','doraemon','shinchan','chhota bheem','motu patlu']
-# print("Accessing the list using positive indexing:")
-# print(cartoon[0])
-# print(cartoon[1])
-# print(cartoon[2])
-# print(cartoon[3])
-# print(cartoon[4])
+cartoon=['Tom and jerry','doraemon','shinchan','chhota bheem','motu patlu']
+print("Accessing the list using positive indexing:")
+print(cartoon[0])
+print(cartoon[1])
+print(cartoon[2])
+print(cartoon[3])
+print(cartoon[4])
 
-# print("Accessing the list using negative indexing:")
-# print(cartoon[-1])
-# print(cartoon[-2])
-# print(cartoon[-3])
-# print(cartoon[-4])
-# print(cartoon[-5])
-
-
-# List=[5,10,2,4,15,6,8,12,20,3]
-# print("List is:",List)
-# print("Length of the list is:",len(List))
-# print("Maximum value in the list is:",max(List))
-# print("Minimum value in the list is:",sum(List))
-# print("Average of the list is:",sum(List)/len(List))
-# print("Sorted list is:",sorted(List))
+print("Accessing the list using negative indexing:")
+print(cartoon[-1])
+print(cartoon[-2])
+print(cartoon[-3])
+print(cartoon[-4])
+print(cartoon[-5])
 
 
-# my_list = [1,2,3,4,5,6,7,8,9,10]
-# print(my_list[:])
-# print(my_list[0:5])
-# print(my_list[::2])
-# print(my_list[5:])
-# print(my_list[-6:-2])
-# print(my_list[::-1])
+List=[5,10,2,4,15,6,8,12,20,3]
+print("List is:",List)
+print("Length of the list is:",len(List))
+print("Maximum value in the list is:",max(List))
+print("Minimum value in the list is:",sum(List))
+print("Average of the list is:",sum(List)/len(List))
+print("Sorted list is:",sorted(List))
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+print(my_list[:])
+print(my_list[0:5])
+print(my_list[::2])
+print(my_list[5:])
+print(my_list[-6:-2])
+print(my_list[::-1])
 
 
 
-# list=["Python","java","c++","Javascipt"]
-# print("The original list is:")
+list=["Python","java","c++","Javascipt"]
+print("The original list is:")
 
 
