695e4c9c9e8d1365da7bb885d7e7b97940e787e2
pavan-kumar-17 <23jr1a43i7@gmail.com>
2026-06-05 09:02:23 +0530

    nested function.py

--- nested function.py
+++ nested function.py
@@ -1,46 +1,46 @@
-# def one():
-#     def two():
-#         print("two")
-#     print("one")
-# # one()
+def one():
+    def two():
+        print("two")
+    print("one")
+# one()
 
 
-# def washhands():
-#     print("washing hands")
-# def servingfood():
-#     print("Serving food ")
-# def eatfood():
-#     washhands()
-#     servingfood()
-#     print("eating food")
-#     washhands()
-# eatfood()  
+def washhands():
+    print("washing hands")
+def servingfood():
+    print("Serving food ")
+def eatfood():
+    washhands()
+    servingfood()
+    print("eating food")
+    washhands()
+eatfood()  
 
-# def one(xyz):
-#     print("First Function "+xyz())
-# def two():
-#     return "Second Function" 
-# one(two)
-
-
-# def add(a,b):
-#     return a+b
-# print(add(5,3))
-
-# add=lambda a,b:a+b
+def one(xyz):
+    print("First Function "+xyz())
+def two():
+    return "Second Function" 
+one(two)
 
 
-# def area():
-#     r=float(input("enter the radius of the circle:"))
-#     area=3.14 * r * r
-#     print("the area of circle is: ",area)
-# area()
+def add(a,b):
+    return a+b
+print(add(5,3))
 
-# square=lambda y:y*y
-# print(square(7))
+add=lambda a,b:a+b
 
-# def total(*num):
-#     print(sum(num))
-# total(2,2,3,4,2,1,3,4,3,56,6,4,5)
+
+def area():
+    r=float(input("enter the radius of the circle:"))
+    area=3.14 * r * r
+    print("the area of circle is: ",area)
+area()
+
+square=lambda y:y*y
+print(square(7))
+
+def total(*num):
+    print(sum(num))
+total(2,2,3,4,2,1,3,4,3,56,6,4,5)
 
 
